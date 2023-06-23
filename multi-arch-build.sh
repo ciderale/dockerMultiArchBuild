024f7f3a5ae4cb40b7d3eb87b84f0db7b62da5c2
Alain Lehmann <alain.lehmann@ergon.ch>
2023-06-23 18:13:28 +0200

    make builds concurrent

--- multi-arch-build.sh
+++ multi-arch-build.sh
@@ -9,5 +9,7 @@ echo "# simple loop over architectures for individual images"
 ARCHS=(arm64 amd64)
 for arch in "${ARCHS[@]}"; do
   tag=$IMG-$arch:$VERSION
-  docker build -t "$tag" --platform "linux/$arch" .
+  docker build -t "$tag" --platform "linux/$arch" . &
 done
+
+wait
